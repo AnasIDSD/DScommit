94d2fe5190fe894c22d7d0aa03257c1d85ae8a20
AnasIDSD <anaskhaldouny@gmail.com>
2022-11-28 10:54:18 +0100

    fin Question 6

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,11 +1,22 @@
 const pug = require('pug');
+const env = require('dotenv')
 const Post=require("../models/post/postModel")
 const user=require("../models/user/userModel")
 
 
 async function getPosts(req,res){
-   //Recupérer tous les posts dans myBlogdb et envoyer index.pug au client
-}
+    env.config({ path:'.env' })
+    const thispost = main().catch(console.error)
+    async function main() {
+    const { MongoClient } = require('mongodb');
+    const client = new MongoClient("mongodb+srv://myblog:31BAakJZVotfPWll@cluster0.ybqupzv.mongodb.net/?retryWrites=true&w=majority")
+    await client.connect();
+    const db = client.db('myblog')
+    const coll = db.collection('posts')
+    const post = await coll.find().toArray()
+    return post
+    }
+    res.render('index', { thispost: await thispost})}
 
 async function getPost(req,res){
     //Recupérer un post definie par son _id dans myBlogdb et envoyer post.pug au client
@@ -14,6 +25,10 @@ async function getPost(req,res){
 async function addPost(req,res){
    //Créer un nouveau post dans myBlogdb et rediriger le client vers /
 }
+async function renderaddPost(req,res){
+
+}
+
 
 async function editPost(req,res){
     //Recupérer un post definie par son _id et renvoyer au client editPost.pug avec les donnée de ce post
@@ -26,4 +41,4 @@ async function deletePost(req,res){
     //Suprimer un post et rediriger le client vers /
 }
 
-module.exports={getPosts,getPost,addPost,updatePost,editPost,deletePost}
+module.exports={getPosts,getPost,addPost,updatePost,editPost,deletePost,renderaddPost}
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,16 @@
 const express=require("express")
-const {getPosts,getPost,addPost,updatePost,editPost,deletePost }=require("../controllers/postsController")
+const {getPosts,getPost,addPost,updatePost,editPost,deletePost,renderaddPost}=require("../controllers/postsController")
 
 const router=express.Router();
 
 // les routes post
+router.get('/', getPosts);
+router.get('/addpost',renderaddPost);
+router.post('/addpost', addPost);
+router.get("/post/:id", getPost)
+router.get("/delete/:id", deletePost)
+router.get("/update/:id", editPost)
+router.post("/update", updatePost)
 
 
 module.exports=router
